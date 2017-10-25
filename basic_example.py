ecbc7db110a13394bec0e7de4668870c3274e67d
Adithi Rao <adithirao@Adithis-MacBook-Pro.local>
2017-10-25 14:39:55 -0400

    version1

--- basic_example.py
+++ basic_example.py
@@ -16,18 +16,38 @@ def update_line(num, data, line):
     line.set_data(data[..., num])
     return line,
 
+
+#buildings
+class Building():
+    x=[]
+    y=[]
+groceries=Building()
+groceries.x=[-0.2+k*0.01 for k in range(40)]
+groceries.y=[0.4 for k in range(40)]
+
+
+
+
+
 fig1 = plt.figure()
 data=np.ndarray((2,25))
 data[0] = np.zeros(25)
 data[1]= np.linspace(0,1, 25)
 l, = plt.plot([], [], 'ro')
+
+plt.plot(0,0.3,'ro')
+plt.plot(groceries.x,groceries.y,'bo')
+plt.plot([0,0],[0,1],'k')
 plt.xlim(-0.5, 0.5)
 plt.ylim(0, 1)
 plt.xlabel('x')
 plt.title('test')
+
 line_ani = animation.FuncAnimation(fig1, update_line, 25, fargs=(data, l),
-                                   interval=25, blit=True)
+                                   interval=50, blit=True)
 
 # To save the animation, use the command: line_ani.save('lines.mp4')
 
 plt.show()
+
+
--- project.py
+++ project.py
@@ -1,1 +1,16 @@
-print("Hello, World")
+import pygame,sys
+from pygame.locals import *
+
+pygame.init()
+
+DISPLAYSURF = pygame.display.set_mode((300,300))
+
+pygame.display.set_caption("Project")
+
+while True:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+	pygame.display.update()
