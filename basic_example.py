6b02620fa993539a7620e585f8c1a544e3a36416
Karna Morey <Karna_Morey@Karnas-MacBook-Pro.local>
2017-10-25 14:13:07 -0400

    Hello Adithi!

---
+++ basic_example.py
@@ -0,0 +1,33 @@
+"""
+=========================
+Simple animation examples
+=========================
+
+This example contains two animations. The first is a random walk plot. The
+second is an image animation.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def update_line(num, data, line):
+    line.set_data(data[..., num])
+    return line,
+
+fig1 = plt.figure()
+data=np.ndarray((2,25))
+data[0] = np.zeros(25)
+data[1]= np.linspace(0,1, 25)
+l, = plt.plot([], [], 'ro')
+plt.xlim(-0.5, 0.5)
+plt.ylim(0, 1)
+plt.xlabel('x')
+plt.title('test')
+line_ani = animation.FuncAnimation(fig1, update_line, 25, fargs=(data, l),
+                                   interval=25, blit=True)
+
+# To save the animation, use the command: line_ani.save('lines.mp4')
+
+plt.show()
