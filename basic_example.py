5ce935fb47522fc0d09b42e8027bc602aa1290e0
Adithi Rao <adithirao@Adithis-MacBook-Pro.local>
2017-10-29 19:01:02 -0400

    i am merging`

--- basic_example.py
+++ basic_example.py
@@ -11,7 +11,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-
 def update_line(num, data, line):
     line.set_data(data[..., num])
     return line,
@@ -22,21 +21,30 @@ class Building():
     x=[]
     y=[]
 groceries=Building()
-groceries.x=[-0.2+k*0.01 for k in range(40)]
-groceries.y=[0.4 for k in range(40)]
-
-
-
-
+houses=Building()
+groceries.x=[0.2,-0.4,-0.1]
+groceries.y=[0.4,0.5,0.6]
+tempx= [(-5+0.1*i)/10 for i in range(0,400,7)]
+houses.y=[]
+#houses.x=[-0.5+0.1*k for k in range(40)]
+for i in range(40):
+#    houses.y=[0.6 for k in range(40)]
+    break
+for k in range(3):
+    yval=0.1*k+0.5
+    tempy=[yval for x in range(len(tempx))]
+    houses.y+=tempy
+    houses.x+=tempx
 
 fig1 = plt.figure()
 data=np.ndarray((2,25))
 data[0] = np.zeros(25)
-data[1]= np.linspace(0,1, 25)
+data[1]= np.linspace(0,1,25)
 l, = plt.plot([], [], 'ro')
 
 plt.plot(0,0.3,'ro')
 plt.plot(groceries.x,groceries.y,'bo')
+plt.plot(houses.x,houses.y,'go')
 plt.plot([0,0],[0,1],'k')
 plt.xlim(-0.5, 0.5)
 plt.ylim(0, 1)
